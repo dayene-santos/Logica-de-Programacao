059d59729a81d17e407cb0156a793fafcd470b3f
DayeneS <dayene-santos@hotmail.com>
2025-03-10 15:52:40 -0300

    Problema Pares Consecutivos - Estrutura Repetitiva/While

--- src/estruturaRepetitiva/Exercicio7.java
+++ src/estruturaRepetitiva/Exercicio7.java
@@ -1,10 +1,34 @@
 package estruturaRepetitiva;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Exercicio7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);	
+		Scanner sc = new Scanner(System.in);
+			
+			int x, soma;
+			
+			System.out.print("Digite um número inteiro: ");
+				x = sc.nextInt();
+				
+			while(x != 0) {
+				if(x % 2 !=0) {
+					x++;
+				}
+				
+				soma = 5 * x + 20;
+				System.out.printf("SOMA = %d\n", soma);
+				
+				System.out.print("Digite um número inteiro: ");
+					x = sc.nextInt();
+			}
+			
+			
+			sc.close();
 	}
 
 }
+
