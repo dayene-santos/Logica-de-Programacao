13a0fb257a418c9b133a8063f73bf77ca8659fcd
DayeneS <dayene-santos@hotmail.com>
2025-03-17 14:27:28 -0300

    Problema Soma_Impares - Estrutura Repetitiva/for

--- src/estruturaRepetitiva/Exercicio9.java
+++ src/estruturaRepetitiva/Exercicio9.java
@@ -1,9 +1,40 @@
+/*  
+Problema "soma_impares" (adaptado de URI 1071) 
+Leia 2 valores inteiros X e Y (em qualquer ordem). A seguir, calcule e mostre a soma dos números 
+impares entre eles.*/ 
+
 package estruturaRepetitiva;
+import java.util.Scanner;
 
 public class Exercicio9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int x, y, soma, troca;
+		
+		System.out.println("Digite dois números: ");
+			x = sc.nextInt();
+			y = sc.nextInt();
+			
+		
+		if(x >y) {
+			troca = x;
+			x = y;
+			y = troca;
+		}
+			
+			soma = 0;
+		for(int i =x+1; i<y; i++) {
+			if(i % 2 != 0) {
+				soma = soma +i;
+			}
+		}
+		
+		System.out.printf("SOMA DOS IMPARES = %d\n", soma);
+		
+		sc.close();
 
 	}
 
